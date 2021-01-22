9e7be5b833f8348800347234b973ad0a43eff0ce
karovda <timmartinmusic@gmail.com>
2021-01-21 19:16:31 -0500

    Added url for all-time-batting

--- baseball/urls.py
+++ baseball/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "baseball"
 urlpatterns = [
-    path("", views.baseball_home_view),
-    path("player/<int:num>", views.player_view),
+    path("", views.baseball_home_view, name="baseball-home"),
+    path("all-time-batting-leaders", views.alltime_batting, name="all-time-batting"),
+    path("player/<int:num>", views.player_view, name="player-detail"),
 ]
