d774ca1548f0f43bf4218525d3a6b94d3d2f4439
karovda <timmartinmusic@gmail.com>
2021-01-21 19:15:04 -0500

    Created alternating hiding display for batting/pitching

---
+++ blog/static/blog/JS/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#select-pitching-stats').addEventListener('click', () => showPitchingStats());
+    document.querySelector('#select-batting-stats').addEventListener('click', () => showBattingStats());
+})
+
+function showPitchingStats() {
+    document.querySelector('#batting-stats').style.display = "none";
+    document.querySelector('#pitching-stats').style.display = "block";
+}
+
+function showBattingStats() {
+    document.querySelector('#batting-stats').style.display = "block";
+    document.querySelector('#pitching-stats').style.display = "none";
+}
