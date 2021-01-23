9a0b6563e19517350a3a3e75a91937ff258e7bb5
karovda <timmartinmusic@gmail.com>
2021-01-22 23:10:00 -0500

    Added logic for all leaderboards

--- baseball/urls.py
+++ baseball/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 app_name = "baseball"
 urlpatterns = [
-    path("", views.baseball_home_view, name="baseball-home"),
-    path("all-time-batting-leaders", views.alltime_batting, name="all-time-batting"),
-    path("player/<int:num>", views.player_view, name="player-detail"),
+    path("", views.baseball_home, name="baseball-home"),
+    path(
+        "<str:when_player>-<str:player_type>-leaders",
+        views.leaderboards,
+        name="leaderboards",
+    ),
+    path("player/<int:num>", views.player_detail, name="player-detail"),
 ]
