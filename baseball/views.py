d9a0f17b695a90b1a6add3bb94bcfd22f61e6452
karovda <timmartinmusic@gmail.com>
2021-01-22 23:09:12 -0500

    Changed batting to leaderboards & added additional logic

--- baseball/views.py
+++ baseball/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.http import Http404
 
 from .models import Batters, CareerStats, Pitchers, PlayerInfo
 
 
-def baseball_home_view(request):
+def baseball_home(request):
     return render(request, "baseball_base.html")
 
 
-def player_view(request, num):
+def player_detail(request, num):
 
     return render(
         request,
@@ -22,15 +23,45 @@ def player_view(request, num):
     )
 
 
-def alltime_batting(request):
-    players = CareerStats.objects.select_related("player").order_by("-bat_career")[:100]
+def leaderboards(request, when_player, player_type):
+    career = False
+    if when_player == "career":
+        career = True
+    elif when_player == "current":
+        career = False
+    else:
+        raise Http404(
+            "Try again with either career or current in /baseball/{whenplayer}-batting-leaders"
+        )
+
+    batting = False
+
+    if player_type == "batting":
+        batting = True
+        if career:
+            players = CareerStats.objects.select_related("player").order_by(
+                "-bat_career"
+            )[:500]
+    elif player_type == "pitching":
+        batting = False
+        if career:
+            players = CareerStats.objects.select_related("player").order_by(
+                "-pit_career"
+            )[:500]
+    else:
+        raise Http404(
+            "Try again with either batting or pitching in /baseball/career-{player_type}-leaders"
+        )
+
     paginator = Paginator(players, 25)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     return render(
         request,
-        "alltime_batting.html",
+        "leaderboards.html",
         {
             "page_obj": page_obj,
+            "batting": batting,
+            "career": career,
         },
     )
